8bd080c4a677a43d171af8c7a40ef84bfd6d77ac
WataRain <81730861+WataRain@users.noreply.github.com>
2021-11-18 18:53:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-def input_strict(allowed_types: tuple, prompt=None):
+def input_strict(allowed_types: list, prompt=None):
     """Input only (a) certain type(s)"""
     while True:
         x = input(prompt)
-        for type in allowed_types:
+        for allowed in allowed_types:
             try:
-                return type(x)
+                return allowed(x)
             except Exception:
                 pass
         print("Please input something castable to one of the following:\n", ", ".join([type.__name__ for type in allowed_types]))
 
 def main():
-    input_strict((int), "Input an int: ")
+    # Test
+    # input_strict
+    for allowed in (int, float, list, bool):
+        foo = input_strict([allowed], "Input something castable to " + allowed.__name__ +": ")
+        print(foo, "is a valid", allowed.__name__)
     return
 
 if __name__ == "__main__":
